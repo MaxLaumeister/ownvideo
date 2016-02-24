45880533688b951a8bc9e4ea0ed6c31784e30197
Maximillian Laumeister <max.laumeister@gmail.com>
2016-02-23 18:41:03 -0800

    Unfinished work on volume persistence

--- js/watch.js
+++ js/watch.js
@@ -30,6 +30,65 @@ function slugify(text)
     .replace(/-+$/, '');            // Trim - from end of text
 }
 
+// Source: https://github.com/m-gagne/limit.js
+
+/**
+ * debounce
+ * @param {integer} milliseconds This param indicates the number of milliseconds
+ *     to wait after the last call before calling the original function.
+ * @param {object} What "this" refers to in the returned function.
+ * @return {function} This returns a function that when called will wait the
+ *     indicated number of milliseconds after the last call before
+ *     calling the original function.
+ */
+Function.prototype.debounce = function (milliseconds, context) {
+    var baseFunction = this,
+        timer = null,
+        wait = milliseconds;
+
+    return function () {
+        var self = context || this,
+            args = arguments;
+
+        function complete() {
+            baseFunction.apply(self, args);
+            timer = null;
+        }
+
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(complete, wait);
+    };
+};
+
+/**
+* throttle
+* @param {integer} milliseconds This param indicates the number of milliseconds
+*     to wait between calls before calling the original function.
+* @param {object} What "this" refers to in the returned function.
+* @return {function} This returns a function that when called will wait the
+*     indicated number of milliseconds between calls before
+*     calling the original function.
+*/
+Function.prototype.throttle = function (milliseconds, context) {
+    var baseFunction = this,
+        lastEventTimestamp = null,
+        limit = milliseconds;
+
+    return function () {
+        var self = context || this,
+            args = arguments,
+            now = Date.now();
+
+        if (!lastEventTimestamp || now - lastEventTimestamp >= limit) {
+            lastEventTimestamp = now;
+            baseFunction.apply(self, args);
+        }
+    };
+};
+
 /* Show the correct playlist in sidebar */
 
 function getURLParameter(name) {
@@ -93,10 +152,18 @@ video.oncanplay = function() {
 
 /* Save video volume */
 
+var prev_volume = 1;
 function saveVideoVolume() {
     var volume = video.volume;
-    console.log(volume);
+    if (volume !== prev_volume) {
+        console.log(volume);
+        // TODO: Save volume here
+    }
+    prev_volume = volume;
 }
 
-video_wrapper.addEventListener("mouseup", saveVideoVolume);
-video_wrapper.addEventListener("mouseleave", saveVideoVolume);
+var sv_throttled = saveVideoVolume.throttle(200);
+
+video_wrapper.addEventListener("mouseleave", sv_throttled);
+video_wrapper.addEventListener("mousemove", sv_throttled);
+window.addEventListener("beforeunload", saveVideoVolume);
